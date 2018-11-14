f4e53ab93ebdaf10b36a36128fae491898d898bc
suzhihua <1079087531@qq.com>
2018-11-14 18:14:11 +0800

    优化nginx模块，将 file 对象合并

--- modules/lb/manifests/config.pp
+++ modules/lb/manifests/config.pp
@@ -2,6 +2,7 @@
 # @see http://nginx.org/en/docs/http/load_balancing.html
 # @see https://docs.nginx.com/nginx/admin-guide/load-balancer/http-load-balancer/
 # @see https://help.aliyun.com/product/27537.html
+# see https://devco.re/blog/2014/06/19/client-ip-detection/
 #
 class lb::config {
 
--- modules/nginx/manifests/config.pp
+++ modules/nginx/manifests/config.pp
@@ -1,5 +1,10 @@
 class nginx::config {
 
+  File {
+    require => Package['nginx'],
+    notify => Service['nginx']
+  }
+
   # file { '/etc/nginx/sites-enabled/default':
   #   ensure => absent,
   #   require => Package['nginx'],
@@ -9,45 +14,28 @@ class nginx::config {
   # conf.d
   file { '/etc/nginx/conf.d/upstream.conf':
     ensure => file,
-    source => 'puppet:///modules/nginx/conf.d/upstream.conf',
-    require => Package['nginx'],
-    notify => Service['nginx']
+    source => 'puppet:///modules/nginx/conf.d/upstream.conf';
   }
 
   # snippets
-  file { '/etc/nginx/snippets/headers.conf':
-    ensure => file,
-    content => template('nginx/snippets/headers.conf.erb'),
-    require => Package['nginx'],
-    notify => Service['nginx']
-  }
-
-  file { '/etc/nginx/snippets/status.conf':
-    ensure => file,
-    source => 'puppet:///modules/nginx/snippets/status.conf',
-    require => Package['nginx'],
-    notify => Service['nginx']
-  }
-
-  file { '/etc/nginx/snippets/cors.conf':
-    ensure => file,
-    content => template('nginx/snippets/cors.conf.erb'),
-    require => Package['nginx'],
-    notify => Service['nginx']
-  }
-
-  file { '/etc/nginx/snippets/pre_release.conf':
-    ensure => file,
-    content => template('nginx/snippets/pre_release.conf.erb'),
-    require => Package['nginx'],
-    notify => Service['nginx']
+  file {
+    '/etc/nginx/snippets/headers.conf':
+      ensure => file,
+      content => template('nginx/snippets/headers.conf.erb');
+    '/etc/nginx/snippets/status.conf':
+      ensure => file,
+      source => 'puppet:///modules/nginx/snippets/status.conf';
+    '/etc/nginx/snippets/cors.conf':
+      ensure => file,
+      content => template('nginx/snippets/cors.conf.erb');
+    '/etc/nginx/snippets/pre_release.conf':
+      ensure => file,
+      content => template('nginx/snippets/pre_release.conf.erb')
   }
 
   # nginx.conf
   file { '/etc/nginx/nginx.conf':
     ensure => file,
-    source => 'puppet:///modules/nginx/nginx.conf',
-    require => Package['nginx'],
-    notify => Service['nginx']
+    source => 'puppet:///modules/nginx/nginx.conf'
   }
 }
